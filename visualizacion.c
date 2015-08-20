e24b520e4b082e791cbc00d2ac24c84becba9b31
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-08-20 12:16:20 -0500

    Creacion de la funcion visualizacion.c

---
+++ visualizacion.c
@@ -0,0 +1,9 @@
+#include "visualizacion.h"
+int contador;
+int visualizacion_registro(registro[]){
+for (contador=0;contador<=11;contador++)
+	{
+		printf("Registro[%d]=%d\n",contador,registro[contador]);
+	}
+return 0;
+}
