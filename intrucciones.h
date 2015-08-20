27d8ab86427a675840710293e6a17a2806dcc213
NicolasArias <nariasm@uqvirtual.edu.co>
2015-08-20 12:53:54 -0500

    Creacion de funciones intrucciones

---
+++ intrucciones.h
@@ -0,0 +1,7 @@
+/**
+* \brief Funcion para realizar la suma con dos registros
+* \param registro[a] Registro para el resultado
+* \param registro[b] Registro de ingreso para sumar
+* \param Num Valor  a sumar
+*/
+int ADD (long int registro[a],long int registro[b], int Num);
