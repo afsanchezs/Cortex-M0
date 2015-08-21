f2021879f07904cdc05d72f2247cfc107eb0d29e
afsanchezs <afsanchezs96@gmail.com>
2015-08-20 22:04:16 -0500

    Se agregaron funciones de operaciones binarias

--- instrucciones.c
+++ instrucciones.c
@@ -4,3 +4,28 @@ int ADD(long int registro[a], long int registro [b], int Num)
 	registro[a]=registro[b]+Num;
 	return registro[a];
 }
+int ADDS(long int registro[a],long int registro[b], int registro[c])
+{
+	registro [a]=registro[b]+registro[c];
+	return registro[a];
+}
+int ORR (long int registro[a],long int registro[b], int registro[c])
+{
+	registro [a]=registro[b] | registro[c];
+	return registro[a];
+}
+int EOR (long int registro[a],long int registro[b], int registro[c])
+{
+	registro [a]=registro[b] ^ registro[c];
+	return registro[a];
+}
+int MOV (long int registro[a],long int registro[b])
+{
+	registro [a]=registro[b];
+	return registro[a];
+}
+int AND (long int registro[a],long int registro[b], int registro[c])
+{
+	registro [a]=registro[b] & registro[c];
+	return registro[a];
+}
