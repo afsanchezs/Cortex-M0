e15bfd634ef5e72f5cc7f71a83c6086edff31fb1
afsanchezs <afsanchezs96@gmail.com>
2015-08-20 22:07:48 -0500

    Se los registros para las funciones

--- instrucciones.h
+++ instrucciones.h
@@ -1,6 +1,6 @@
-int ADD (long int registro[a],long int registro[b], int Num);
-int ADDS(long int registro[a],long int registro[b], int Num);
-int ORR (long int registro[a],long int registro[b], int Num);
-int EOR (long int registro[a],long int registro[b], int Num);
-int MOV (long int registro[a],long int registro[b]);
-int AND (long int registro[a],long int registro[b], int Num);
+int ADD (int registro[a],int registro[b], int Num);
+int ADDS(int registro[a],int registro[b], int registro[c]);
+int ORR (int registro[a],int registro[b], int registro[c]);
+int EOR (int registro[a],int registro[b], int registro[c]);
+int MOV (int registro[a],int registro[b]);
+int AND (int registro[a],int registro[b], int registro[c]);
