6167245f823631e9d03fbc76bb32234c62aaaa56
mayurablue <mayurablue@hotmail.com>
2015-08-21 17:23:16 -0500

    cambio por variable num

--- instrucciones.h
+++ instrucciones.h
@@ -4,7 +4,7 @@
 * \param registro[b] Registro de ingreso para sumar
 * \param Num Valor  a sumar
 */
-int ADD (int R1,int R2, int R3);
+int ADD (int R1,int R2, int Num);
 int ADDS(int R1,int R2, int R3);
 int ORR (int R1,int R2, int R3;
 int EOR (int R1,int R2, int R3);
