90e1373a14ac04a1f2efedf979cf8ab020083044
NicolasArias <nariasm@uqvirtual.edu.co>
2015-08-21 15:18:40 -0500

    Se borro archivo defectuoso

--- intrucciones.h
+++
@@ -1,7 +0,0 @@
-/**
-* \brief Funcion para realizar la suma con dos registros
-* \param registro[a] Registro para el resultado
-* \param registro[b] Registro de ingreso para sumar
-* \param Num Valor  a sumar
-*/
-int ADD (long int registro[a],long int registro[b], int Num);
