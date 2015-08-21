c714f3ae37b959c3d739006194ac7fe20f295bf9
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-08-20 22:39:34 -0500

    Se empieza a comentar el código para la documentar

--- instrucciones.h
+++ instrucciones.h
@@ -1,3 +1,8 @@
+/**
+* \brief Funcion para realizar la suma con dos registros
+* \param registro[a] Registro para el resultado
+* \param registro[b] Registro de ingreso para sumar
+* \param Num Valor  a sumar
 int ADD (int registro[a],int registro[b], int Num);
 int ADDS(int registro[a],int registro[b], int registro[c]);
 int ORR (int registro[a],int registro[b], int registro[c]);
