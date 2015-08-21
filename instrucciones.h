a6e5b705d792b080da0fb8452ce19afd45ee79bf
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-08-20 21:58:33 -0500

    Create instrucciones.h
    Se crean las funciones correspondientes a las instrucciones.

---
+++ instrucciones.h
@@ -0,0 +1,6 @@
+int ADD (long int registro[a],long int registro[b], int Num);
+int ADDS(long int registro[a],long int registro[b], int Num);
+int ORR (long int registro[a],long int registro[b], int Num);
+int EOR (long int registro[a],long int registro[b], int Num);
+int MOV (long int registro[a],long int registro[b]);
+int AND (long int registro[a],long int registro[b], int Num);
