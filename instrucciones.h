494230dcec84429b700e475f743012ab4b2c3ef3
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-08-21 22:12:10 -0500

    Documentación de los "return" de casa funcion

--- instrucciones.h
+++ instrucciones.h
@@ -3,6 +3,7 @@
 * \param R1 Registro para el resultado.
 * \param R2 Registro de ingreso para sumar.
 * \param Num Valor  a sumar.
+* \return Resultado de la operacion sumar
 */
 int ADD (int R1,int R2, int Num);
 /**
@@ -10,6 +11,7 @@ int ADD (int R1,int R2, int Num);
 * \param R1 Registro para el resultado.
 * \param R2 Primer registro de ingreso a sumar.
 * \param R3 Segundo registro de ingreso a sumar.
+* \return Resultado de la operacion sumar
 */
 int ADDS(int R1,int R2, int R3);
 /**
@@ -17,6 +19,7 @@ int ADDS(int R1,int R2, int R3);
 * \param R1 Registro para el resultado
 * \param R2 Primer registro de ingreso para ORR.
 * \param R3 Segundo registro de ingreso para ORR.
+* \return Resultado de la operacion ORR
 */
 int ORR (int R1,int R2, int R3;
 /**
@@ -24,12 +27,14 @@ int ORR (int R1,int R2, int R3;
 * \param R1 Registro para el resultado.
 * \param R2 Primer registro de ingreso para EOR
 * \param R3 Segundo registro de ingreso para EOR.
+* \return Resultado de la operacion EOR
 */
 int EOR (int R1,int R2, int R3);
 /**
 * \brief Funcion para realizar MOV (reemplazar el valor de un registro en otro)
 * \param R1 Registro para el resultado
 * \param R2 Registro de ingreso para  MOV (posicion en la cual se va a mover el valor del registro)
+* \return Resultado de la operacion Mov
 */
 int MOV (int R1,int R2);
 /**
@@ -37,5 +42,6 @@ int MOV (int R1,int R2);
 * \param R1 Registro para el resultado
 * \param R2 Primer registro de ingreso para AND
 * \param R3 Segundo registro de ingreso para AND
+* \return Resultado de la operacion AND
 */
 int AND (int R1,int R2, int R3);
