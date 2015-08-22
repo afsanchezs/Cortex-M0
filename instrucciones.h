cab2d4afcd1e40d23c0d8ffefeb6692e9366d433
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-08-21 22:49:06 -0500

    Arreglo de la documentación,

--- instrucciones.h
+++ instrucciones.h
@@ -3,7 +3,7 @@
 * \param R1 Registro para el resultado.
 * \param R2 Registro de ingreso para sumar.
 * \param Num Valor  a sumar.
-* \return Retorna como resultado de la operacion sumar un entero
+* \return Retorna como resultado  un entero
 */
 int ADD (int R1,int R2, int Num);
 /**
@@ -11,7 +11,7 @@ int ADD (int R1,int R2, int Num);
 * \param R1 Registro para el resultado.
 * \param R2 Primer registro de ingreso a sumar.
 * \param R3 Segundo registro de ingreso a sumar.
-* \return Retorna como resultado de la operacion sumar un entero
+* \return Retorna como resultado un entero 
 */
 int ADDS(int R1,int R2, int R3);
 /**
@@ -19,7 +19,7 @@ int ADDS(int R1,int R2, int R3);
 * \param R1 Registro para el resultado
 * \param R2 Primer registro de ingreso para ORR.
 * \param R3 Segundo registro de ingreso para ORR.
-* \return Retorna como resultado de la operacion ORR un entero
+* \return Retorna como resultado un entero
 */
 int ORR (int R1,int R2, int R3;
 /**
@@ -27,14 +27,14 @@ int ORR (int R1,int R2, int R3;
 * \param R1 Registro para el resultado.
 * \param R2 Primer registro de ingreso para EOR
 * \param R3 Segundo registro de ingreso para EOR.
-* \return Retorna como resultado de la operacion EOR un entero
+* \return Retorna como resultado  un entero
 */
 int EOR (int R1,int R2, int R3);
 /**
 * \brief Funcion para realizar MOV (reemplazar el valor de un registro en otro)
 * \param R1 Registro para el resultado
 * \param R2 Registro de ingreso para  MOV (posicion en la cual se va a mover el valor del registro)
-* \return Retorna como resultado de la operacion Mov un entero
+* \return Retorna como resultado un entero en la posicion deseada
 */
 int MOV (int R1,int R2);
 /**
@@ -42,7 +42,14 @@ int MOV (int R1,int R2);
 * \param R1 Registro para el resultado
 * \param R2 Primer registro de ingreso para AND
 * \param R3 Segundo registro de ingreso para AND
-* \return Retorna como resultado de la operacion AND un entero
+* \return Retorna como resultado un entero
 */
 int AND (int R1,int R2, int R3);
+/**
+ * \brief Funcion para realizar la resta entre un registro y un valor
+ * \param R1 Registro para el resultado
+ * \param R2 Primer registro de ingreso a sumar
+ * \param R3 Segundo registro de ingreso a sumar
+ * \return Retorna como resultdo un entero
+ */
 int SUB (int R1,int R2, int R3);
