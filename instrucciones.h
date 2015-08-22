a75d9d186cb34c0fdceb9ebce009f39f39228f92
NicolasArias <nariasm@uqvirtual.edu.co>
2015-08-21 19:07:32 -0500

    Se organizo parte de la documentacion de la libreria instrucciones.h

--- instrucciones.h
+++ instrucciones.h
@@ -1,12 +1,41 @@
 /**
-* \brief Funcion para realizar la suma con dos registros
-* \param registro[a] Registro para el resultado
-* \param registro[b] Registro de ingreso para sumar
-* \param Num Valor  a sumar
+* \brief Funcion para realizar la suma de un registro y un valor.
+* \param R1 Registro para el resultado.
+* \param R2 Registro de ingreso para sumar.
+* \param Num Valor  a sumar.
 */
 int ADD (int R1,int R2, int Num);
+/**
+* \brief Funcion para realizar la suma con dos registros.
+* \param R1 Registro para el resultado.
+* \param R2 Primer registro de ingreso a sumar.
+* \param R3 Segundo registro de ingreso a sumar.
+*/
 int ADDS(int R1,int R2, int R3);
+/**
+* \brief Funcion para realizar ORR con dos registros
+* \param R1 Registro para el resultado
+* \param R2 Primer registro de ingreso para ORR.
+* \param R3 Segundo registro de ingreso para ORR.
+*/
 int ORR (int R1,int R2, int R3;
+/**
+* \brief Funcion para realizar EOR  con dos registros
+* \param R1 Registro para el resultado.
+* \param R2 Primer registro de ingreso para EOR
+* \param R3 Segundo registro de ingreso para EOR.
+*/
 int EOR (int R1,int R2, int R3);
+/**
+* \brief Funcion para realizar MOV (reemplazar el valor de un registro en otro)
+* \param R1 Registro para el resultado
+* \param R2 Registro de ingreso para  MOV (posicion en la cual se va a mover el valor del registro)
+*/
 int MOV (int R1,int R2);
+/**
+* \brief Funcion para realizar AND dos registros
+* \param R1 Registro para el resultado
+* \param R2 Primer registro de ingreso para AND
+* \param R3 Segundo registro de ingreso para AND
+*/
 int AND (int R1,int R2, int R3);
