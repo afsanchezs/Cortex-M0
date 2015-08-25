5af7b964b639b4f6f88835c5676b9fdcc4909499
mayurablue <mayurablue@hotmail.com>
2015-08-25 08:24:43 -0500

    cambio variable rdm por rd

--- instrucciones.h
+++ instrucciones.h
@@ -1,55 +1,55 @@
 /**
 * \brief Funcion para realizar la suma de un registro y un valor.
-* \param Rdm Registro para el resultado.
+* \param Rd Registro para el resultado.
 * \param Rn Registro de ingreso para sumar.
 * \param Num Valor  a sumar.
 * \return Retorna como resultado  un entero
 */
-int ADD (int Rdm,int Rn, int Num);
+int ADD (int Rd,int Rn, int Num);
 /**
 * \brief Funcion para realizar la suma con dos registros.
-* \param Rdm Registro para el resultado.
+* \param Rd Registro para el resultado.
 * \param Rn Primer registro de ingreso a sumar.
 * \param Rm Segundo registro de ingreso a sumar.
 * \return Retorna como resultado un entero 
 */
-int ADDS(int Rdm,int Rn, int Rm);
+int ADDS(int Rd,int Rn, int Rm);
 /**
 * \brief Funcion para realizar ORR con dos registros
-* \param Rdm Registro para el resultado
+* \param Rd Registro para el resultado
 * \param Rn Primer registro de ingreso para ORR.
 * \param Rm Segundo registro de ingreso para ORR.
 * \return retorna como resultado un entero
 */
-int ORR (int Rdm,int Rn, int Rm;
+int ORR (int Rd,int Rn, int Rm;
 /**
 * \brief Funcion para realizar EOR  con dos registros
-* \param Rdm Registro para el resultado.
+* \param Rd Registro para el resultado.
 * \param Rn Primer registro de ingreso para EOR
 * \param Rm Segundo registro de ingreso para EOR.
 * \return Retorna como resultado  un entero
 */
-int EOR (int Rdm,int Rm, int Rn);
+int EOR (int Rd,int Rm, int Rn);
 /**
 * \brief Funcion para realizar MOV (reemplazar el valor de un registro en otro)
-* \param Rdm Registro para el resultado
+* \param Rd Registro para el resultado
 * \param Rn Registro de ingreso para  MOV (posicion en la cual se va a mover el valor del registro)
 * \return Retorna como resultado un entero en la posicion deseada
 */
-int MOV (int Rdm,int Rn);
+int MOV (int Rd,int Rn);
 /**
 * \brief Funcion para realizar AND dos registros
-* \param Rdm Registro para el resultado
+* \param Rd Registro para el resultado
 * \param Rn Primer registro de ingreso para AND
 * \param Rm Segundo registro de ingreso para AND
 * \return Retorna como resultado un entero
 */
-int AND (int Rdm,int Rn, int Rm);
+int AND (int Rd,int Rn, int Rm);
 /**
  * \brief Funcion para realizar la resta entre un registro y un valor
- * \param Rdm Registro para el resultado
+ * \param Rd Registro para el resultado
  * \param Rn Primer registro de ingreso a sumar
  * \param Rm Segundo registro de ingreso a sumar
  * \return Retorna como resultdo un entero
  */
-int SUB (int Rdm,int Rn, int Rm);
+int SUB (int Rd,int Rn, int Rm);
