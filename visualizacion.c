69c91f00d1232d9aa345d5f27ebb93aca7ad801c
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-08-27 07:26:49 -0500

    Se organizo la estructura del codigo.

--- visualizacion.c
+++ visualizacion.c
@@ -1,9 +1,10 @@
 #include "visualizacion.h"
 int contador;
-int visualizacion_registro(registro[]){
-for (contador=0;contador<=11;contador++)
-	{
-		printf("Registro[%d]=%d\n",contador,registro[contador]);
-	}
-return 0;
+int visualizacion_registro(registro[]) 
+{
+	for (contador=0;contador<=11;contador++)
+		{
+			printf("Registro[%d]=%d\n",contador,registro[contador]);
+		}
+	return 0;
 }
