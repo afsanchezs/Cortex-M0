eb6c4d8235c457af2eec7005873d7fdb2198503f
afsanchezs <afsanchezs96@gmail.com>
2015-08-27 08:49:49 -0500

    Declaración de variables

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 */
 int main(void)
 {
-	int registro[]={0,1,2,3,4,5,6,7,8,9,10,11}, i;
+	int registro[]={0,1,2,3,4,5,6,7,8,9,10,11},R, i, N, Z, C, V, *Bn, *Bz, *Bc, *Bv;
+	Bn=&N; Bz=&Z; Bc=&C; Bv=&V; R=&registro[];
 	visualizacion_registro(registro[]);
 	return 0;
 }
