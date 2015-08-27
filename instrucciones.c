26ce7edbc7355409627df630c96d76fa0dd5c570
NicolasArias <nariasm@uqvirtual.edu.co>
2015-08-27 08:08:37 -0500

    Anexo de funcion banderas

--- instrucciones.c
+++ instrucciones.c
@@ -34,3 +34,40 @@ int SUB (int Rd,int Rn, int Rm)
 	Rd=Rn-Rm;
 	return Rd;
 }
+int Bandera (int Rd, int Rn, int Rm)
+{
+if (Rd>127)
+	{
+		N=1;
+	}
+if (Rd=(128 || 256)
+	{
+		Z=1;
+	}
+if (Rd>255)
+	{
+		C=1;
+	}
+if (Rd>127)
+	{
+		if ((Rn<127)&&(Rm<127))
+			{
+				V=1;
+			}
+		else 
+			{
+				V=0;
+			}
+	}
+else 
+	{
+		if ((Rn>127)&&(Rm>127))
+			{
+				V=1;
+			}
+		else 
+			{
+				V=0;
+			}		
+	}
+}
