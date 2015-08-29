9595143511f29f120528626f6070de2b68a21ab2
afsanchezs <afsanchezs96@gmail.com>
2015-08-29 00:19:41 -0500

    Update

--- visualizacion.c
+++ visualizacion.c
@@ -1,7 +1,7 @@
 #include "visualizacion.h"
 void visualizacion_registro(unsigned long *R) 
 {
-	int i;  // i es equivalente a un contador// 
+	unsigned long i;  // i es equivalente a un contador// 
 	for (i=0;i<=11;i++)
 		{
 			printf("Registro[%lu]=%lu\t",i,R[i]);  // Linea del codigo que muestra el registro ingresado// 
