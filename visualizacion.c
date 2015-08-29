b374e56b8d87623ed04089c3888da68429bf7c06
afsanchezs <afsanchezs96@gmail.com>
2015-08-29 00:10:42 -0500

    Cambio de asignación de variable para el printf

--- visualizacion.c
+++ visualizacion.c
@@ -4,7 +4,7 @@ void visualizacion_registro(unsigned long *R)
 	int i;  // i es equivalente a un contador// 
 	for (i=0;i<=11;i++)
 		{
-			printf("Registro[%d]=%d\t",i,R[i]);  // Linea del codigo que muestra el registro ingresado// 
+			printf("Registro[%lu]=%lu\t",i,R[i]);  // Linea del codigo que muestra el registro ingresado// 
 		}
 	return;
 }
