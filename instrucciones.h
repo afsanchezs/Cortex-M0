56de19e7e6e78bd57b4a66f201c13a27bf2def79
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-09-01 08:29:43 -0500

    Organización de la documentación.

--- instrucciones.h
+++ instrucciones.h
@@ -53,127 +53,148 @@ void AND (unsigned long *Rd,unsigned long Rn, unsigned long Rm);
 * \return void
 */
 void SUB (unsigned long *Rd,unsigned long Rn, unsigned long Rm);
-/** brief Funcion generadoras de banderas.
+/** 
+* \ brief Funcion generadoras de banderas.
 * \ param Rd Resultado de la operacion.
 * \ param Rn Primer valor de operacion.
 * \ param Rm Segundo valor de operacion.
 * \ return void
 */
 void Bandera (unsigned long *Rd,unsigned long Rn, unsigned long Rm);
-/** brief Funcion desplazamiento hacia la izquierda ingresando un numero a sumar.
+/** 
+* \ brief Funcion desplazamiento hacia la izquierda ingresando un numero a sumar.
 * \ param Rd Resultado de la operacion.
 * \ param Rn Valor del registro a sumar.
 * \ param Num Valor a sumar.
 * \ return void.
 */
 void LSL(unsigned long *Rd, unsigned long Rn, unsigned long Num);
-/** biref Funcion desplazamiento hacia la izquierda con dos registros.
+/** 
+* \ biref Funcion desplazamiento hacia la izquierda con dos registros.
 * \ param Rd Resultado de la operacion.
 * \ param Rn Valor del registro a sumar.
 * \ return void.
 */
 void LSLS(unsigned long *Rd, unsigned long Rn);
-/** brief Funcion desplazamiento hacia la derecha con un numero a sumar.
+/** 
+* \ brief Funcion desplazamiento hacia la derecha con un numero a sumar.
 * \ param Rd Resultado de la operacion
 * \ param Rn Valor del registro a sumar.
 * \ param Num Valor a sumar.
 * \ return void 
 */ 
 void LSR(unsigned long *Rd , unsigned long Rn ,unsigned long Num);
-/** biref Funcion desplazamiento hacia la Derecha con dos registros.
+/** 
+* \ biref Funcion desplazamiento hacia la Derecha con dos registros.
 * \ param Rd Resultado de la operacion.
 * \ param Rn Valor del registro a sumar.
 * \ return void.
 */
 void LSDS(unsigned long *Rd, unsigned long Rn);
-/** brief Funcion para rotar.
+/** 
+* \ brief Funcion para rotar.
 * \ param Rdn Resultado de la operacion.
 * \ param Rn Numero a rotar.
 * \ return void 
 */
 void ROR(unsigned long *Rd, unsigned long Rn);
-/** brief Funcion para desplazar aritmeticamente hacia la derecha con un registro y y un numero.
+/** 
+* \ brief Funcion para desplazar aritmeticamente hacia la derecha con un registro y y un numero.
 * \ param Rd Resultado de la operacion.
 * \ param Rn Valor del registro a sumar.
 * \ param Num Valor a sumar.
 * \ return void.
 */
 void ASR(unsigned long *Rd, unsigned long Rn, unsigned long Num);
-/** brief Funcion para despalazar aritmeticamente hacia la derecha con dos registros.
+/** 
+* \ brief Funcion para despalazar aritmeticamente hacia la derecha con dos registros.
 * \ param Rd Resultado de la operacion.
 * \ param Rn numero a trasladar.
 * \ return void.
 */ 
 void ASRS(unsigned long *Rd, unsigned long Rn);
-/** brief Funcion que hace una and de 1 bit y con Rm negado.
+/** 
+* \ brief Funcion que hace una and de 1 bit y con Rm negado.
 * \ param Rd Resultado de la operacion 
 * \ param Rn Registro a negar.
 * \ return void.
 */ 
 void BICS(unsigned long *Rd, unsigned long Rn);
-/** brief Funcion que modifica las banderas sin guardar el resultado 
+/** 
+* \ brief Funcion que modifica las banderas sin guardar el resultado 
 * \ param Rn Primer registro a operar.
 * \ param Rm Segundo registro a operar.
 * \ return void.
 */
 void CMN(unsigned long Rn, unsigned long Rm);
-/** biref Funcion que modifica las banderas comparando los registros.
+/** 
+* \ biref Funcion que modifica las banderas comparando los registros.
 * \ param Rn Primer registro a comparar.
 * \ param Rm Segundo regsitro a comparar.
 * \ return void.
 */
 void CMP(unsigned long Rn, unsigned long Rm);
-/** brief Funcion que modifica banderas comparando un registro y un numero.
+/** 
+* \ brief Funcion que modifica banderas comparando un registro y un numero.
 * \ param Rn Registro a comparar.
 * \ param Num Numero para comparar con el registro.
 * \ return void.
 */
 void CMPS(unsigned long Rn,unsigned long Num);
-/** brief Funcion Multiplica dos registros, sin signo y almacena los 32 bits menos significativos del resultado en el regsitro de destino.
+/** 
+* \ brief Funcion Multiplica dos registros, sin signo y almacena los 32 bits menos significativos del resultado en el regsitro de destino.
 * \ param Rd Registro para el resultado de la operacion.
 * \ param Rn Primer valor del reistro a multiplicar.
 * \ param Rm Segundo valor del registro a multiplicar.
 * \ return void.
 */
 void MUL(unsigned long *Rd, unsigned long Rn, unsigned long Rm);
-/** brief funcion para hacer el complemento a un registro.
+/** 
+* \ brief funcion para hacer el complemento a un registro.
 * \ param Rd Resultado de la operacion.
 * \ retrn void.
 */ 
 void MVN(unsigned long *Rd);
-/** brief Funcion que hace un retardo.
+/** 
+* \ brief Funcion que hace un retardo.
 * \return void.
 */
 void NOP(unsigned long *Rd, unsigned long Rn);
-/** brief Funcion para cambiar el orden de los bits
+/** 
+* \ brief Funcion para cambiar el orden de los bits
 * \ param Rb Registro a modificar.
 * \ return void.
 */
 void REV(unsigned long *Rd, unsigned long Rn);
-/** brief Funcion para modificar la posicion de grupos de 16 bits.
+/** 
+* \ brief Funcion para modificar la posicion de grupos de 16 bits.
 * \ param Rd Registro resultante.
 * \ param Rn Registro a modificar.
 * \ return void.
 */
 void REV16(unsigned long *Rd, unsigned long Rn);
-/** Funcion para modificar la posicion de a ocho bits
+/** 
+* \ brief Funcion para modificar la posicion de a ocho bits
 * \ param Rd Numero a modificar.
 * \ return void
 */ 
 void REVSH(unsigned long *Rd, unsigned long Rn);
-/** brief Funcion para obtener el complemento a dos de un numero
+/** 
+* \ brief Funcion para obtener el complemento a dos de un numero
 * \ param Rb Registro al cual se le hara el complemento.
 * \ return void. 
 */
 void RSB(unsigned long *Rd);
-/** brief Funcion para restar con Carry
+/** 
+* \ brief Funcion para restar con Carry
 * \ param Rd Registro para el resultado de la operacion.
 * \ param Rn Primer valor a operar
 * \ param Rm Segundo valor a operar
 * \ return void.
 */ 
 void SBC(unsigned long *Rd, unsigned long Rn, unsigned long Rm);
-/** brief Funcion que hace una AND bit a bit, pero solo modifica las banderas.
+/** 
+* \ brief Funcion que hace una AND bit a bit, pero solo modifica las banderas.
 * \ parm Rn Primer valor ingresado para hacer la operacion.
 * \ pram Rm Segundo valor ingresado para hacer la operacion.
 * \ return void.
