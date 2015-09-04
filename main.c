21818b17e61355b5925fcc800c4a4cd899c768bf
afsanchezs <afsanchezs96@gmail.com>
2015-09-03 21:40:53 -0500

    Corrección fallos

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 int main()
 {
 	int registro[]={0,1,2,3,4,5,6,7,8,9,10,11},*R;
-	R=&registro[];
-	visualizacion_registro(registro[]);
+	R=&registro;
+	visualizacion_registro(registro);
 	return 0;
 }
