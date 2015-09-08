c2996be1da402f8286789736f3fd34d2570a9d3f
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-09-07 20:44:56 -0500

    Se agregaron comentarios

--- instrucciones.c
+++ instrucciones.c
@@ -2,37 +2,39 @@
 int i; //Contador
 void ADD(unsigned long *Rd, unsigned long Rn, unsigned long Num)
 {
-	*Rd=Rn+Num;
+	*Rd=Rn+Num;  // Esta operación es la suma entre un registro y
+		     // un numero.
 	return;
 }
 void ADDS(unsigned long *Rd, unsigned long Rn,unsigned long Rm)
 {
-	*Rd=Rn+Rm;
+	*Rd=Rn+Rm;	// Esta operación representa la suma entre dos registros.
 	return;
 }
 void ORR (unsigned long *Rd,unsigned long Rn, unsigned long Rm)
 {
-	*Rd=Rn | Rm;
+	*Rd=Rn | Rm;  //El caracter "|", es el encargado de realizar la operacion 
+		      //de la compuerta lógica OR, la operación es realizada entre dos registros.
 	return;
 }
 void EOR (unsigned long *Rd,unsigned long Rn, unsigned long Rm)
 {
-	*Rd=Rn ^ Rm;
-	return;
+	*Rd=Rn ^ Rm;	// El caracter "^" es el encargado de realizar la operación
+	return;		// de la compuerta lógica de XOR, la operacion es realizada entre dos registros.
 }
 void MOV (unsigned long *Rd,unsigned long Rn)
 {
-	*Rd=Rn;
+	*Rd=Rn;		// Esta operación realiza una copia de un registro sin alterar el registro de ingreso.
 	return;
 }
 void AND (unsigned long *Rd,unsigned long Rn, unsigned long Rm)
 {
-	*Rd=Rn & Rm;
-	return;
+	*Rd=Rn & Rm;	// El caracter "&" es el encargado de realizar la operacion
+	return;		// de la compuerta lógica de AND, la operación es realizada entre dos registros.
 }
 void SUB (unsigned long *Rd,unsigned long Rn, unsigned long Rm)
 {
-	*Rd=Rn-Rm;
+	*Rd=Rn-Rm;	// Esta operación es para realizar la resta entre dos regitros 
 	return;
 }
 /*void Bandera (unsigned long Rd, unsigned long Rn, unsigned long Rm)
@@ -74,8 +76,8 @@ else
 }*/
 void LSL(unsigned long *Rd, unsigned long Rn, unsigned long Num)
 {
-	*Rd=Rn<<Num;
-	return;
+	*Rd=Rn<<Num; 		 
+	return;			
 }
 void LSLS(unsigned long *Rd, unsigned long Rn)
 {
@@ -84,8 +86,8 @@ void LSLS(unsigned long *Rd, unsigned long Rn)
 }
 void LSR(unsigned long *Rd, unsigned long Rn, unsigned long Num)
 {
-	*Rd=Rn>>Num;
-	return;
+	*Rd=Rn>>Num;		
+	return;			
 }
 void LSRS(unsigned long *Rd, unsigned long Rn)
 {
