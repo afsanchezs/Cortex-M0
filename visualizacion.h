70bd414eab265b37d76680c74ae440144b9b3109
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-09-11 21:35:57 -0500

    Se agregaron las nuevas variables a la funcion

--- visualizacion.h
+++ visualizacion.h
@@ -1,6 +1,10 @@
 /**
 *\ brief Funcion para visualizar los registros
 *\ param *R Puntero que señala la posicion de los registros almacenados.
+*\ param  N Corresponde a la bandera Negativo.
+*\ param  Z Corresponde a la bandera Cero.
+*\ param  C Corresponde a la bandera de Acarreo.
+*\ param  V Corresponde a la bandera de Sobre Flujo.
 *\ return void 
 */
 void visualizacion_registro(unsigned long *R, unsigned long *N, unsigned long *Z, unsigned long *C, unsigned long *V);
