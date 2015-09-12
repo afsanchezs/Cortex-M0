317178f6a3f88d6ac37ebcc4646256d6a6b07419
afsanchezs <afsanchezs96@gmail.com>
2015-09-11 21:20:44 -0500

    Adicion de banderas para visualizacion

--- main.c
+++ main.c
@@ -7,8 +7,9 @@
 */
 int main()
 {
-	unsigned long registro[]={0,1,2,3,4,5,6,7,8,9,10,11},*R;
+	unsigned long registro[]={0,1,2,3,4,5,6,7,8,9,10,11},*R,*N,*Z,*C,*V;
+	N=0;Z=0;C=0;V=0;
 	R=registro;
-	visualizacion_registro(R);
+	visualizacion_registro(R,N,Z,C,V);
 	return 0;
 }
