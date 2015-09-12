7a4576dae7f5d7fc36ccaa07578d311f45ad35b3
afsanchezs <afsanchezs96@gmail.com>
2015-09-11 21:23:16 -0500

    Declaracion para visualizacion de banderas

--- visualizacion.h
+++ visualizacion.h
@@ -3,4 +3,4 @@
 *\ param *R Puntero que señala la posicion de los registros almacenados.
 *\ return void 
 */
-void visualizacion_registro(unsigned long *R);
+void visualizacion_registro(unsigned long *R, unsigned long *N, unsigned long *Z, unsigned long *C, unsigned long *V);
