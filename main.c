098db8edf1a1afbfd64f9d284aca42bb6b2a6622
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-09-11 21:58:44 -0500

    Comentario complementario.

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 int main()
 {
 	unsigned long registro[]={0,1,2,3,4,5,6,7,8,9,10,11},*R,*N,*Z,*C,*V;
-	N=0;Z=0;C=0;V=0;
+	N=0;Z=0;C=0;V=0;	// Se inicializan las banderas es estos valores para corroborar el funcionamiento de la interfaz
 	R=registro;
 	visualizacion_registro(R,N,Z,C,V);
 	return 0;
