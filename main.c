80f6fbe9af6f6a21530fa2c3dc34bd0737aa568b
afsanchezs <afsanchezs96@gmail.com>
2015-09-19 08:47:41 -0500

    Se pone a correr paso a paso

--- main.c
+++ main.c
@@ -8,56 +8,41 @@
 */
 int main()
 {
-	unsigned long registro[]={0,1,2,3,4,5,6,7,8,9,10,11},*Banderas,lines,contador=0,*R;
-	FILE* fp;
+	unsigned long registro[]={0,1,2,3,4,5,6,7,8,9,10,11},Banderas[4]={0},contador=0,*R,i,num_instructions;
+	char** instructions;
+	ins_t read;
+	instruction_t instruction;
+	/*FILE* fp;*/
 	R=registro;
 	//------- No modificar ------//	
-		int i, num_instructions;
-		ins_t read;
-		char** instructions;
-		instruction_t instruction;
-	
 		num_instructions = readFile("code.txt", &read);
-		if(num_instructions==-1)
+/*		if(num_instructions==-1)
 			return 0;
 	
 		if(read.array==NULL)
-			return 0;	
+			return 0;	*/
 	
-		instructions = read.array; /* Arreglo con las instrucciones */
-	//---------------------------//	
-	fp = fopen("code.txt", "r");
-	lines = countLines(fp)-1;
+		instructions = read.array;/* Arreglo con las instrucciones */
+	//---------------------------//		
+	while (contador<num_instructions)
+	{
+		R[contador]=10;
+		instruction = getInstruction(instructions[contador]); // Instrucción en la posición 0
+		decodeInstruction(instruction); // Debe ser modificada de acuerdo a cada código
+		visualizacion_registro(R,Banderas);
+		contador++;
+		
+	}
+/*	fp = fopen("code.txt", "r");
+	lines = countLines(fp)-1;*/
 		//------- No modificar ------//	
 		/* Libera la memoria reservada para las instrucciones */
 		for(i=0; i<num_instructions; i++){
 			free(read.array[i]);
 		}	
 		free(read.array);
-		//---------------------------//
-	while (contador>lines)
-	{	
-		visualizacion_registro(R,Banderas);
-		contador++;
-		
-	}
-	/* Ejemplo de uso 
-		Llama la función que separa el mnemonico y los operandos
-		Llama la instrucción que decodifica y ejecuta la instrucción
-	*/
-	// Esto debe ser ciclico para la lectura de todas las instrucciones, de acuerdo
-	// al valor del PC (Program Counter)
-	instruction = getInstruction(instructions[0]); // Instrucción en la posición 0
-	decodeInstruction(instruction); // Debe ser modificada de acuerdo a cada código
-
-
-
-	//------- No modificar ------//	
-	/* Libera la memoria reservada para las instrucciones */
-	for(i=0; i<num_instructions; i++){
-		free(read.array[i]);
-	}	
-	free(read.array);
-	//---------------------------//
+		//---------------------------//	
+	/*instruction = getInstruction(instructions[contador]); // Instrucción en la posición 0
+	decodeInstruction(instruction)*/; // Debe ser modificada de acuerdo a cada código
 	return 0;
 }
