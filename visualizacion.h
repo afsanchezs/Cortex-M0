bd1e1677bcd378c0f018aea52ff860f5dda44566
afsanchezs <afsanchezs96@gmail.com>
2015-09-18 23:21:31 -0500

    Actualizacion Banderas

--- visualizacion.h
+++ visualizacion.h
@@ -1,10 +1,7 @@
 /**
 *\ brief Funcion para visualizar los registros
 *\ param *R Puntero que señala la posicion de los registros almacenados.
-*\ param  N Corresponde a la bandera Negativo.
-*\ param  Z Corresponde a la bandera Cero.
-*\ param  C Corresponde a la bandera de Acarreo.
-*\ param  V Corresponde a la bandera de Sobre Flujo.
+*\ param  *Banderas Corresponde a las banderas Cero, Negativo, Acarreo y Sobreflujo.
 *\ return void 
 */
-void visualizacion_registro(unsigned long *R, unsigned long *N, unsigned long *Z, unsigned long *C, unsigned long *V);
+void visualizacion_registro(unsigned long *R, unsigned long *Banderas);
