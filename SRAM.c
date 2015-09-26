47b36e741eedd5dcc78d19e589a9c1e0b6f86b7b
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-09-26 10:06:48 -0500

    se crea la funcion PUSH

---
+++ SRAM.c
@@ -0,0 +1,19 @@
+# include "RSAM.h"
+
+void PUSH(uint32_t *registro,uint32_t *R1,uint32_t *R2,uint32_t *R3)
+{
+
+int *Mem[16]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},i,n;  //Se inicia una memoria vacia
+uint32_t address, *SP=&Mem[1];     // Se crea la variable address y el puntero SP se coloca apuntanda a la primer direccion
+address=SP-12
+n=3         //Numero de registros ingresados
+for (i=0;i<=n;i++)
+{
+     Mem[anddress+4]=R[i];
+     address=address-4;
+     SP=SP-12
+
+}
+
+
+}
