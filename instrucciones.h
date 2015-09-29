57e62846b6f1576b3df0c0b32136d0d49ff525ab
afsanchezs <afsanchezs96@gmail.com>
2015-09-28 21:17:21 -0500

    Actualizacion Parámetros.

--- instrucciones.h
+++ instrucciones.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "flags.h"
 /**
 * \brief Funcion para realizar la suma de un registro y un valor.
 * \param Rd Registro para el resultado.
@@ -6,7 +7,7 @@
 * \param Num Valor  a sumar.
 * \return void
 */
-void ADD (uint32_t *Rd,uint32_t *Rn, uint32_t Num);
+void ADD (uint32_t *Rd,uint32_t *Rn, uint32_t Num, uint32_t *R, uint32_t Banderas);
 /**
 * \brief Funcion para realizar la suma con dos registros.
 * \param Rd Registro para el resultado.
@@ -14,7 +15,7 @@ void ADD (uint32_t *Rd,uint32_t *Rn, uint32_t Num);
 * \param Rm Segundo registro de ingreso a sumar.
 * \return void
 */
-void ADDS(uint32_t *Rd,uint32_t *Rn, uint32_t *Rm);
+void ADDS(uint32_t *Rd,uint32_t *Rn, uint32_t *Rm, uint32_t *R, uint32_t Banderas);
 /**
 * \brief Funcion para realizar ORR con dos registros
 * \param Rd Registro para el resultado
@@ -22,7 +23,7 @@ void ADDS(uint32_t *Rd,uint32_t *Rn, uint32_t *Rm);
 * \param Rm Segundo registro de ingreso para ORR.
 * \return void 
 */
-void ORR (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm);
+void ORR (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm, uint32_t *R, uint32_t Banderas);
 /**
 * \brief Funcion para realizar EOR  con dos registros
 * \param Rd Registro para el resultado.
@@ -30,14 +31,14 @@ void ORR (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm);
 * \param Rm Segundo registro de ingreso para EOR.
 * \return void
 */
-void EOR (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm);
+void EOR (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm, uint32_t *R, uint32_t Banderas);
 /**
 * \brief Funcion para realizar MOV (reemplazar el valor de un registro en otro)
 * \param Rd Registro para el resultado
 * \param Rn Registro de ingreso para  MOV (posicion en la cual se va a mover el valor del registro)
 * \return void
 */
-void MOV (uint32_t *Rd,uint32_t *Rn);
+void MOV (uint32_t *Rd,uint32_t *Rn, uint32_t *R, uint32_t Banderas);
 /**
 * \brief Funcion para realizar AND dos registros
 * \param Rd Registro para el resultado
@@ -45,7 +46,7 @@ void MOV (uint32_t *Rd,uint32_t *Rn);
 * \param Rm Segundo registro de ingreso para AND
 * \return void
 */
-void AND (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm);
+void AND (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm, uint32_t *R, uint32_t Banderas);
 /**
 * \brief Funcion para realizar la resta entre un registro y un valor
 * \param Rd Registro para el resultado
@@ -53,7 +54,7 @@ void AND (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm);
 * \param Rm Segundo registro de ingreso a sumar
 * \return void
 */
-void SUB (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm);
+void SUB (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion generadoras de banderas.
 * \param Rd Resultado de la operacion.
@@ -62,7 +63,7 @@ void SUB (uint32_t *Rd,uint32_t *Rn, uint32_t *Rm);
 * \param Banderas Es un arreglo donde se guardaran los resutados de da las banderas.
 * \return void
 */
-void Bandera (uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *Banderas);
+void Bandera (uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t Banderas);
 /** 
 * \brief Funcion desplazamiento hacia la izquierda ingresando un numero a sumar.
 * \param Rd Resultado de la operacion.
@@ -70,14 +71,14 @@ void Bandera (uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *Banderas);
 * \param Num Valor a sumar.
 * \return void.
 */
-void LSL(uint32_t *Rd, uint32_t *Rn, uint32_t Num);
+void LSL(uint32_t *Rd, uint32_t *Rn, uint32_t Num, uint32_t *R, uint32_t Banderas);
 /** 
 * \biref Funcion desplazamiento hacia la izquierda con dos registros.
 * \param Rd Resultado de la operacion.
 * \param Rn Valor del registro a sumar.
 * \return void.
 */
-void LSLS(uint32_t *Rd, uint32_t *Rn);
+void LSLS(uint32_t *Rd, uint32_t *Rn, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion desplazamiento hacia la derecha con un numero a sumar.
 * \param Rd Resultado de la operacion
@@ -85,21 +86,21 @@ void LSLS(uint32_t *Rd, uint32_t *Rn);
 * \param Num Valor a sumar.
 * \return void 
 */
-void LSR(uint32_t *Rd , uint32_t *Rn ,uint32_t Num);
+void LSR(uint32_t *Rd , uint32_t *Rn , uint32_t Num, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion desplazamiento hacia la Derecha con dos registros.
 * \param Rd Resultado de la operacion.
 * \param Rn Valor del registro a sumar.
 * \return void.
 */
-void LSRS(uint32_t *Rd, uint32_t *Rn);
+void LSRS(uint32_t *Rd, uint32_t *Rn, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion para rotar.
 * \param Rdn Resultado de la operacion.
 * \param Rn Numero a rotar.
 * \return void 
 */
-void ROR(uint32_t *Rd, uint32_t *Rn);
+void ROR(uint32_t *Rd, uint32_t *Rn, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion para desplazar aritmeticamente hacia la derecha con un registro y y un numero.
 * \param Rd Resultado de la operacion.
@@ -107,42 +108,42 @@ void ROR(uint32_t *Rd, uint32_t *Rn);
 * \param Num Valor a sumar.
 * \return void.
 */
-void ASR(uint32_t *Rd, uint32_t *Rn, uint32_t Num);
+void ASR(uint32_t *Rd, uint32_t *Rn, uint32_t Num, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion para despalazar aritmeticamente hacia la derecha con dos registros.
 * \param Rd Resultado de la operacion.
 * \param Rn numero a trasladar.
 * \return void.
 */ 
-void ASRS(uint32_t *Rd, uint32_t *Rn);
+void ASRS(uint32_t *Rd, uint32_t *Rn, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion que hace una and de 1 bit y con Rm negado.
 * \param Rd Resultado de la operacion 
 * \param Rn Registro a negar.
 * \return void.
 */ 
-void BICS(uint32_t *Rd, uint32_t *Rn);
+void BICS(uint32_t *Rd, uint32_t *Rn, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion que modifica las banderas sin guardar el resultado 
 * \param Rn Primer registro a operar.
 * \param Rm Segundo registro a operar.
 * \return void.
 */
-void CMN(uint32_t *Rn, uint32_t *Rm);
+void CMN(uint32_t *Rn, uint32_t *Rm, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion que modifica las banderas comparando los registros.
 * \param Rn Primer registro a comparar.
 * \param Rm Segundo regsitro a comparar.
 * \return void.
 */
-void CMP(uint32_t *Rn, uint32_t *Rm);
+void CMP(uint32_t *Rn, uint32_t *Rm, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion que modifica banderas comparando un registro y un numero.
 * \param Rn Registro a comparar.
 * \param Num Numero para comparar con el registro.
 * \return void.
 */
-void CMPS(uint32_t *Rn,uint32_t Num);
+void CMPS(uint32_t *Rn,uint32_t Num, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion Multiplica dos registros, sin signo y almacena los 32 bits menos significativos del resultado en el regsitro de destino.
 * \param Rd Registro para el resultado de la operacion.
@@ -150,43 +151,43 @@ void CMPS(uint32_t *Rn,uint32_t Num);
 * \param Rm Segundo valor del registro a multiplicar.
 * \return void.
 */
-void MUL(uint32_t *Rd, uint32_t Rn, uint32_t Rm);
+void MUL(uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief funcion para hacer el complemento a un registro.
 * \param Rd Resultado de la operacion.
 * \return void.
 */ 
-void MVN(uint32_t *Rd,uint32_t *Rn);
+void MVN(uint32_t *Rd,uint32_t *Rn, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion que hace un retardo.
 * \return void.
 */
-void NOP(void);
+void NOP(uint32_t *R);
 /** 
 * \brief Funcion para cambiar el orden de los bits
 * \param Rb Registro a modificar.
 * \return void.
 */
-void REV(uint32_t *Rd, uint32_t *Rn);
+void REV(uint32_t *Rd, uint32_t *Rn, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion para modificar la posicion de grupos de 16 bits.
 * \param Rd Registro resultante.
 * \param Rn Registro a modificar.
 * \return void.
 */
-void REV16(uint32_t *Rd, uint32_t *Rn);
+void REV16(uint32_t *Rd, uint32_t *Rn, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion para modificar la posicion de a ocho bits
 * \param Rd Numero a modificar.
 * \return void
 */ 
-void REVSH(uint32_t *Rd, uint32_t *Rn);
+void REVSH(uint32_t *Rd, uint32_t *Rn, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion para obtener el complemento a dos de un numero
 * \param Rb Registro al cual se le hara el complemento.
 * \return void. 
 */
-void RSB(uint32_t *Rd);
+void RSB(uint32_t *Rd, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion para restar con Carry
 * \param Rd Registro para el resultado de la operacion.
@@ -194,11 +195,11 @@ void RSB(uint32_t *Rd);
 * \param Rm Segundo valor a operar
 * \return void.
 */ 
-void SBC(uint32_t *Rd, uint32_t *Rn, uint32_t *Rm);
+void SBC(uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *R, uint32_t Banderas);
 /** 
 * \brief Funcion que hace una AND bit a bit, pero solo modifica las banderas.
 * \param Rn Primer valor ingresado para hacer la operacion.
 * \param Rm Segundo valor ingresado para hacer la operacion.
 * \return void.
 */
-void TST (uint32_t *Rd, uint32_t *Rn);
+void TST (uint32_t *Rd, uint32_t *Rn, uint32_t *R, uint32_t Banderas);
