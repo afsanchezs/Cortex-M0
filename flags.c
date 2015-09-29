14f129bda48a9b8987c3ec9f5d96416b081a7c78
afsanchezs <afsanchezs96@gmail.com>
2015-09-29 08:12:06 -0500

    Modificacion Banderas

--- flags.c
+++ flags.c
@@ -1,5 +1,5 @@
 #include "flags.h"
-void Bandera (uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *Banderas) 
+void BanderaN (uint32_t *Rd, uint32_t *Banderas) 
 {				// La variable Bandera es un vector que contiene el resultado de las banderas "Bandera=[N,Z,C,V]"
 	if (*Rd>2147483647)	// Donde 2147483647 es el mayor numero con signo
 	{
@@ -9,6 +9,11 @@ void Bandera (uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *Banderas)
 	{
 		*(Banderas+0)=0;// Indica que si el registro no es mayor a 2147483647 la bandera N no se activa y se almacena un 0
 	}
+	return;
+}
+
+void BanderaZ (uint32_t *Rd, uint32_t *Banderas) 
+{
 	if (*Rd==0)
 	{
 		*(Banderas+1)=1; // Indica que si el registro es igual a 4294967296 o a 2147483648 la bandera Z se activa almacenando un 1
@@ -17,6 +22,10 @@ void Bandera (uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *Banderas)
 	{
 		*(Banderas+1)=0;// Indica que si el registro es diferente a 4294967296 o a 2147483648 la bandera Z no se activa almacenando un 0
 	}
+	return;
+}
+void BanderaC (uint32_t *Rd, uint32_t *Banderas) 	
+{
 	if (*Rd>4294967295)
 	{
 		*(Banderas+2)=1; //Indica que si el registro es mayor a 4294967295 la bandera C se activa, almacenando un 1.
@@ -25,6 +34,10 @@ void Bandera (uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *Banderas)
 	{
 		*(Banderas+2)=0; //Indica que si el registro es menor a 4294967295 la bandera C no se activa, almacenando un 0.
 	}
+	return;
+}	
+void BanderaV (uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *Banderas) 
+{
 	if (*Rd>2147483647)
 	{
 		if ((*Rn<2147483647)&&(*Rm<2147483647))
@@ -47,4 +60,5 @@ void Bandera (uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *Banderas)
 			*(Banderas+3)=0;
 		}		
 	}
+	return;
 }
