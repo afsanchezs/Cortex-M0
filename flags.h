b9a79a40c0bddd7f879922a14f5bc8ba63e6c19a
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-09-28 21:58:07 -0500

    Documentación de la función bandera.

--- flags.h
+++ flags.h
@@ -1,2 +1,9 @@
 #include <stdint.h>
+/**
+ * \Funcion para cambiar las banderas
+ * \param Rd Variable que almacena el resultado de las operaciones
+ * \param Rn Primer valor a operar
+ * \param Rm Segundo valor a operar
+ * \param Banderas Vector donde se almacenara el resultado de la modificacion de las banderas.
+ */ 
 void Bandera (uint32_t *Rd, uint32_t *Rn, uint32_t *Rm, uint32_t *Banderas);
