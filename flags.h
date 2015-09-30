7a19ce1855bff1c9522f1684218c4f9f348cbd6e
NicolasAriasM <nariasm@uqvirtual.edu.co>
2015-09-29 19:35:12 -0500

    Documentación de las banderas.

--- flags.h
+++ flags.h
@@ -1,12 +1,27 @@
 #include <stdint.h>
 /**
- * \Funcion para cambiar las banderas
- * \param Rd Variable que almacena el resultado de las operaciones
- * \param Rn Primer valor a operar
- * \param Rm Segundo valor a operar
- * \param Banderas Vector donde se almacenara el resultado de la modificacion de las banderas.
+ * \Funcion para modificar la bandera de negativo (N)
+ * \param Rd Variable que contiene el resultado de las operaciones
+ * \param B Punntero en el cual se almacenaran las banderas.
  */ 
 void BanderaN (uint32_t *Rd, uint32_t *B);
-void BanderaZ (uint32_t *Rd, uint32_t *B); 
+/**
+ * \Funcion para modificar la bandera de cero (Z)
+ * \param Rd Variable que contiene el resultado de las operaciones
+ * \param B Punntero en el cual se almacenaran las banderas.
+ */ 
+void BanderaZ (uint32_t *Rd, uint32_t *B);
+/**
+ * \Funcion para modificar la bandera de carry (C)
+ * \param Rd Variable que contiene el resultado de las operaciones
+ * \param B Punntero en el cual se almacenaran las banderas.
+ */ 
 void BanderaC (uint32_t *Rd, uint32_t *B); 
+/**
+ * \Funcion para modificar la bandera de sobre-flujo (V)
+ * \param Rd Variable que contiene el resultado de las operaciones
+ * \param Rn Variable con el primer operando.
+ * \param Rm Variable con el segundo operando.
+ * \param B Punntero en el cual se almacenaran las banderas.
+ */ 
 void BanderaV (uint32_t *Rd, uint32_t Rn, uint32_t Rm, uint32_t *B);
