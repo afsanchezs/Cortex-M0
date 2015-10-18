7af4a24af18ede6945213e4be19c7457253a755a
afsanchezs <afsanchezs96@gmail.com>
2015-10-18 01:19:15 -0500

    Funciones de Carga y Almacenamiento

--- SRAM.c
+++ SRAM.c
@@ -52,35 +52,31 @@ void POP(uint8_t *SP,uint32_t *R,uint8_t *RAM,uint8_t  registers_list[])
 
 void LDR(uint32_t *R,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM)
 {
-	RAM[Rn+Rm]=(uint8_t)(*Rd);
-	RAM[Rn+Rm+1]=(uint8_t)(*Rd>>8);
-	RAM[Rn+Rm+2]=(uint8_t)(*Rd>>16);
-	RAM[Rn+Rm+3]=(uint8_t)(*Rd>>24);
+	*Rd=(RAM[Rn+Rm])+(RAM[Rn+Rm+1]<<8)+(RAM[Rn+Rm+2]<<16)+(RAM[Rn+Rm+3]<<24);
 	R[PC]++;
 }
 
 void LDRB(uint32_t *R,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM)
 {
-	*Rd=Rn+Rm;
+	*Rd=RAM[Rn+Rm];
 	R[PC]++;
 }
 
 void LDRH(uint32_t *R,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM)
 {
-	*Rd=Rn+Rm;
-	
+	*Rd=RAM[Rn+Rm]+(RAM[Rn+Rm+1]<<8);	
 	R[PC]++;
 }
 
 void LDRSB(uint32_t *R,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM)
 {
-	*Rd=(uint32_t)(Rn+Rm);
+	*Rd=(uint32_t)(RAM[Rn+Rm]);
 	R[PC]++;
 }
 
 void LDRSH(uint32_t *R,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM)
 {
-	*Rd=(uint32_t)(Rn+Rm);
+	*Rd=(uint32_t)(RAM[Rn+Rm])+(uint32_t)(RAM[Rn+Rm+1]<<8);
 	R[PC]++;
 }
 
