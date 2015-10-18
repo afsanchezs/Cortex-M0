b4fbde8a02b9ca55e7bd3b51da3aa8ce7ed498b0
afsanchezs <afsanchezs96@gmail.com>
2015-10-17 21:05:16 -0500

    Declaración Funciones de Carga y Almacenamiento

--- SRAM.h
+++ SRAM.h
@@ -24,3 +24,11 @@ uint8_t BitCount(uint8_t  registers_list[]);
 * \ return void
 */ 
 void POP(uint8_t *SP,uint32_t *R,uint8_t *RAM,uint8_t  registers_list[]);
+void LDR(uint32_t *R,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM);
+void LDRB(uint32_t *R,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM);
+void LDRH(uint32_t *R,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM);
+void LDRSB(uint32_t *R,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM);
+void LDRSH(uint32_t *R,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM);
+void STR(uint8_t *RAM,uint32_t *Registro,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM);
+void STRB(uint8_t *RAM,uint32_t *Registro,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM);
+void STRH(uint8_t *RAM,uint32_t *Registro,uint32_t *Rd,uint32_t Rn,uint32_t Rm,uint8_t *RAM);
